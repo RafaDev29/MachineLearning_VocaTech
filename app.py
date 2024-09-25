5341913b54a9875dc86107f7fafb3204be8e15fb
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-09-25 00:11:49 -0500

    integración con el entramiento ML

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS  # Importar CORS
 from src.modules.test_basic.routes import test_basic_bp  # Importamos el blueprint de TestBasic
 
 app = Flask(__name__)
 
+# Habilitar CORS en la app
+CORS(app)
+
 # Registrar el blueprint del módulo TestBasic
 app.register_blueprint(test_basic_bp, url_prefix='/api/v1/test_basic')
- 
+
 @app.errorhandler(404)
 def not_found(e):
     return {"message": "Resource not found", "status": False, "data": None}, 404
